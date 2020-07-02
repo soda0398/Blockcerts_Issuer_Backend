6952c1c5335813f07162ec7d8028684149b96b94
soda0398 <skaieda@yahoo.com>
2020-07-02 11:48:08 +0900

    test post port

--- bc_server.rb
+++ bc_server.rb
@@ -10,5 +10,9 @@ get '/api/issuer_info' do
   content_type :json
   #{hello: "helloworld"}.to_json
   File.read("./public/issuer.json");
+end
+
+post '/api/issuer_info', provides: :json do
+  params = JSON.parse request.body.read
 
 end
